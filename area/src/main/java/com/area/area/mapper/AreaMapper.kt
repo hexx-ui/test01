c90ed3b70ed728755f82a664bccf41d25a8d0328
liyue <412046761@qq.com>
2020-07-27 14:57:07 +0800

    liyue: Mybatis-plus连mysql数据源demo 未全完成

---
+++ area/src/main/java/com/area/area/mapper/AreaMapper.kt
@@ -0,0 +1,17 @@
+package com.area.area.mapper
+
+import com.area.area.entity.Area
+import com.baomidou.mybatisplus.core.mapper.BaseMapper
+
+
+
+
+
+/**
+ * @description: AreaMapper
+ * @author: liyue
+ * @date: 2020/7/27 13:55
+ */
+open interface AreaMapper : BaseMapper<Area>{
+
+}
